3ae524c447eaa606c0c7b5fa899d2c1937acc1c3
Simon Aebli <simon.aebli@tamedia.ch>
2019-09-10 17:58:03 +0200

    add some documentation

--- src/receiver/receiver.js
+++ src/receiver/receiver.js
@@ -1,22 +1,39 @@
 const frames = {};
 
-const register = (notify, init) => {
+/**
+ * Register callbacks for an iframe
+ * 
+ * @param {function} notify callback to notify on message receival
+ * @param {function} refresh callback that get triggered if a message without ID is received
+ * @returns an iframe id
+ */
+const register = (notify, refresh) => {
     const id = "autofit_" + Math.random().toString(36).substr(2, 8);
 ​
     if(!frames[id]) {
-        frames[id] = {notify, init};
+        frames[id] = {notify, refresh};
         return id;
     }
     
     return register();
 };
 ​
+/**
+ * remove registered callbacks for an iframe
+ * 
+ * @param {string} id iframe id
+ */
 const unregister = (id) => {
     frames[id] && delete frames[id];
 };
 ​
-function run(e) {
-    const {id, ...props} = e.data;
+/**
+ * internal message event listener
+ * 
+ * @param {object} event message event
+ */
+function run(event) {
+    const {id, ...props} = event.data;
 
     if(id) {
         if(frames[id]) {
@@ -27,11 +44,14 @@ function run(e) {
     } else {
         // one of the iframes lost its ID
         Object.keys(frames).forEach(id => {
-            frames[id] && frames[id].init(id);
+            frames[id] && frames[id].refresh(id);
         });
     }
 }
 
+/**
+ * start listening for post message events
+ */
 const init = () => {
     window.addEventListener("message", run);
 };
